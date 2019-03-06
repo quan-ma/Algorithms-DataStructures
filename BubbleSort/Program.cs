17198c843b970c16b9dc813ff8b8532adaeffcbf
quan.ma2018 <quan.ma2018@gmail.com>
2019-03-05 21:42:09 -0500

    bubble sort

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -6,7 +6,37 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] Numbers = { 3, 2, 5, 1, 4 };
+            int count = 0;
+            int temp;
+            bool swap;
+
+            //bubble sort
+            do
+            {
+                swap = false;
+                for (int i = 0; i < Numbers.Length - 1; i++)
+                {
+                    if (Numbers[i] > Numbers[i + 1])
+                    {
+                        temp = Numbers[i];
+                        Numbers[i] = Numbers[i + 1];
+                        Numbers[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+            } while (swap == true);
+
+            //print
+            Console.Write("Sorted: {");
+            foreach (var Number in Numbers)
+            {
+                Console.Write(Number);
+                if (Numbers.Length - 1 > count)
+                    Console.Write(", ");
+                count++;
+            }
+            Console.WriteLine("}");
         }
     }
 }
